86574fcfead581495cab04d929ac3d94cd28fb53
Michael Kosmicki <michaelkosmicki@yahoo.com>
2019-06-13 20:23:59 -0400

    Basic toDo function built

--- app.js
+++ app.js
@@ -6,45 +6,38 @@ const bodyParser = require("body-Parser");
 
 const app = express();
 
+var items = ["Buy food", "eat food", "Eat more"];
+
+
+
+
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res) {
+
   var today = new Date();
-  var day = "";
-  var currentDay = today.getDay();
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("Error: Current day is equal to: " + currentDay);
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  }
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {
-    kindOfDay: day
-  });
+  res.render("list", {kindOfDay: day,newListItems: items});
 
 });
+
+  app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+
+  });
+
+
 
 
 
