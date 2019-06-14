5c42c44f749ff497c50afc595a26dc3a9a250cc6
Michael Kosmicki <michaelkosmicki@yahoo.com>
2019-06-13 21:24:47 -0400

    styled toDo list with css

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ let items = ["Buy food", "eat food", "Eat more"];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.get("/", function(req, res) {
 
   let today = new Date();
