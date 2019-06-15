cd673e44df3ec74239975eada40a2e4b9f56153d
Michael Kosmicki <michaelkosmicki@yahoo.com>
2019-06-15 00:49:42 -0400

    moved date for the title into a separate module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 const express = require("express");
 const bodyParser = require("body-Parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -16,15 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get("/", function(req, res) {
 
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
 
   res.render("list", {listTitle: day,newListItems: items});
 
@@ -48,6 +41,7 @@ app.get("/", function(req, res) {
   });
 
 app.get("/work", function(req, res){
+
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
