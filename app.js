2514ff582ffb3041bd36b2176dce9469487c750d
Michael Kosmicki <michaelkosmicki@yahoo.com>
2019-06-15 00:03:01 -0400

    created ejs layouts

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-Parser");
 const app = express();
 
 let items = ["Buy food", "eat food", "Eat more"];
-
+let workItems = [];
 
 
 
@@ -26,21 +26,40 @@ app.get("/", function(req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day,newListItems: items});
+  res.render("list", {listTitle: day,newListItems: items});
 
 });
 
   app.post("/", function(req, res) {
-    let item = req.body.newItem;
-    items.push(item);
 
-    res.redirect("/");
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+      items.push(item);
+
+      res.redirect("/");
+    }
+
 
   });
 
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
+app.get("/about", function(req, res){
+  res.render("about");
+});
 
-
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
